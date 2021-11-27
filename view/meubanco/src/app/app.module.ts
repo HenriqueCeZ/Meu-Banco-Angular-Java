e2a958ce72a91c33110bab0b1c5b6e0efe967ce5
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-27 11:53:30 -0300

    feat: declarando components

---
+++ view/meubanco/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
+import { SaqueComponent } from './pages/saque/saque.component';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExtratoComponent,
+    SaqueComponent,
+    DepositoComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
