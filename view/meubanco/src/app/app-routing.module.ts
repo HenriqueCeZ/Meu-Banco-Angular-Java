afa2dd74fb2ef76e39de94c13bda75b8b3ea9d99
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-27 11:52:53 -0300

    feat : adicionando os paths

---
+++ view/meubanco/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SaqueComponent } from './pages/saque/saque.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent, pathMatch: "full"},
+  {path: 'extrato', component:ExtratoComponent, pathMatch: "full"},
+  {path: 'deposito', component:DepositoComponent, pathMatch: "full"},
+  {path: 'saque', component:SaqueComponent, pathMatch: "full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
