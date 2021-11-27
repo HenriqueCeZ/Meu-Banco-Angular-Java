c7e2038d5c97e747dc50d39c536de89fb4ad7ca8
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-27 11:46:35 -0300

    feat: criação do component saque

---
+++ view/meubanco/src/app/pages/saque/saque.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saque',
+  templateUrl: './saque.component.html',
+  styleUrls: ['./saque.component.css']
+})
+export class SaqueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
