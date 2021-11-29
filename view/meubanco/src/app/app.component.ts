8ef7ed82d486c4aa8bb2544144043a0d389b8ed8
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-29 20:15:41 -0300

    feat: implementação da função ngAfterViewInit do toggle sidebar

--- view/meubanco/src/app/app.component.ts
+++ view/meubanco/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meubanco';
+  ngAfterViewInit(){
+    (function($) {
+      "use strict";
+
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this instanceof HTMLAnchorElement && this.href === path) {
+                  $(this).addClass("active");
+              }
+          });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+}
 }
