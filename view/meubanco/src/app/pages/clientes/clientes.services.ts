e073aae11a2e46734a464a0cff0b18b4948271a2
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-29 20:07:27 -0300

    feat: criando clienteservices

--- view/meubanco/src/app/pages/clientes/clientes.services.ts
+++ view/meubanco/src/app/pages/clientes/clientes.services.ts
@@ -18,12 +18,5 @@ export class ClientesService {
     cliente.ativo = false;
     return cliente;
   }
-  setClient() : Cliente{
-    let  cliente: Cliente = new Cliente();
-    cliente.nome = ""
-    cliente.cpf = "";
-    cliente.observacoes = ""
-    cliente.ativo = false;
-    return cliente;
-  }
+
 }
