caa595c7799c74e6679611454d8b3b46e239d0c1
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-29 23:16:15 -0300

    feat: correção de textos nas páginas

--- view/meubanco/src/app/pages/clientes/cadastrar-clientes/cadastrar-clientes.component.ts
+++ view/meubanco/src/app/pages/clientes/cadastrar-clientes/cadastrar-clientes.component.ts
@@ -12,21 +12,18 @@ export class CadastrarClientesComponent implements OnInit {
 
   cliente!: Cliente ;
 
-  checkoutForm = this.formBuilder.group({
-
-  });
-
-  constructor(private formBuilder: FormBuilder, private clienteService: ClientesService) {
+  constructor() {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
   }
 
+
   onSubmit(): void {
 
-    alert("Cliente com nome: "+this.cliente.nome+"Cliente com cpf: "+this.cliente.cpf+"Cliente observações: "+this.cliente.observacoes+"Cliente com status: "+this.cliente.ativo)
-    this.checkoutForm.reset();
+    alert("nome: "+this.cliente.nome+"cpf: "+this.cliente.cpf+"observações: "+this.cliente.observacoes+"Active: "+this.cliente.ativo)
+
   }
 
 
