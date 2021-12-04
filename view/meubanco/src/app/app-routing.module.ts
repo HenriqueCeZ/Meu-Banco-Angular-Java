1210136e560a0cb8c67a0fa6391f383dad8697ba
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-04 13:16:47 -0300

    feat: adicionando o path de contas

--- view/meubanco/src/app/app-routing.module.ts
+++ view/meubanco/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ExtratoComponent } from './pages/extrato/extrato.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SaqueComponent } from './pages/saque/saque.component';
 import { CadastrarClientesComponent } from './pages/clientes/cadastrar-clientes/cadastrar-clientes.component';
+import { ContasComponent } from './pages/contas/contas.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent, pathMatch: "full"},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: 'deposito', component:DepositoComponent, pathMatch: "full"},
   {path: 'saque', component:SaqueComponent, pathMatch: "full"},
   {path: 'clientes', component:ClientesComponent, pathMatch: "full"},
+  {path: 'contas', component:ContasComponent, pathMatch: "full"},
   {path: 'cadastrar', component:CadastrarClientesComponent, pathMatch: "full"}
 ];
 
