fffc5b79574bbbbb918708b98eacb9bd70c80606
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-04 17:43:30 -0300

    feat: implementando o método post de saque

--- view/meubanco/src/app/services/contas.service.ts
+++ view/meubanco/src/app/services/contas.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IContas } from '../interfaces/contas';
+import { ISaqueDeposito } from '../interfaces/saqueDeposito';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,10 @@ export class ContasService {
 
   listarTodasConta(){
     return this.http.get<IContas[]>(this.api)
-}
+
+  }
+  saque(saque: ISaqueDeposito) {
+    return this.http.post(`${this.api}/saque`, saque);
+  }
+
 }
