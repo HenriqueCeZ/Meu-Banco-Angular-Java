7aaac311572698f54d1d750640e184ced11cc005
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-07 00:31:08 -0300

    feat: implementando o component de deposito o formGroup e o método de depositar

--- view/meubanco/src/app/pages/deposito/deposito.component.ts
+++ view/meubanco/src/app/pages/deposito/deposito.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContasService } from 'src/app/services/contas.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { ISaqueDeposito } from 'src/app/interfaces/saqueDeposito';
 @Component({
   selector: 'app-deposito',
   templateUrl: './deposito.component.html',
   styleUrls: ['./deposito.component.css']
 })
 export class DepositoComponent implements OnInit {
+  formGroup: FormGroup = new FormGroup({
+    agencia: new FormControl('', Validators.required),
+    numeroConta: new FormControl('', Validators.required),
+    valor: new FormControl(null, Validators.required),
+  });
 
-  constructor() { }
+
+
+  constructor(private contasService: ContasService, private router: Router ) {
+
+   }
 
   ngOnInit(): void {
   }
 
+
+  depositar() {
+    const deposito : ISaqueDeposito = this.formGroup.value
+    this.contasService.deposito(deposito).subscribe(clienteApi =>{
+      Swal.fire("Transação realizada!", "Deposito realizado com sucesso!","success")
+      console.log(clienteApi)
+      this.router.navigate(['/contas'])
+    },error =>{
+      console.log(error)
+    })
+    this.formGroup.reset()
+
+
 }
+
+  }
+
+
+
+
