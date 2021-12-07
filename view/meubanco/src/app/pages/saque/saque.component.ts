9a9daa09690cf2d3dc0cb7aca8802445665e6773
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-07 00:31:58 -0300

    feat: implementando o component de saque com o método sacar e o formgroup

--- view/meubanco/src/app/pages/saque/saque.component.ts
+++ view/meubanco/src/app/pages/saque/saque.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContasService } from 'src/app/services/contas.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { ISaqueDeposito } from 'src/app/interfaces/saqueDeposito';
 
 @Component({
   selector: 'app-saque',
@@ -8,19 +12,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SaqueComponent implements OnInit {
 
-  formGroup: FormGroup = new FormGroup({
-    agencia: new FormControl('', Validators.required),
-    numeroConta: new FormControl('', Validators.required),
-    valor: new FormControl('', Validators.required),
-  });
-
-  constructor() { }
+  constructor(private contasService: ContasService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  formGroup: FormGroup = new FormGroup({
+    agencia: new FormControl('', Validators.required),
+    numeroConta: new FormControl('', Validators.required),
+    valor: new FormControl(null, Validators.required),
+  });
+
   sacar() {
+    const saque: ISaqueDeposito = this.formGroup.value;
+    this.contasService.saque(saque).subscribe(clienteApi =>{
+      Swal.fire("Transação realizada!", "Saque realizado com sucesso!","success")
+      console.log(clienteApi)
+      this.router.navigate(['/contas'])
+    },error =>{
+      console.log(error)
+    })
+    this.formGroup.reset()
 
-  }
 
+
+}
 }
