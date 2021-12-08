57e2c7204f4b5c016054651a4c1523907070b4e4
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-08 17:28:11 -0300

    feat: implementando o método deletar contas

--- view/meubanco/src/app/pages/contas/contas.component.ts
+++ view/meubanco/src/app/pages/contas/contas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IContas } from 'src/app/interfaces/contas';
 import { ContasService } from 'src/app/services/contas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contas',
@@ -19,4 +20,32 @@ export class ContasComponent implements OnInit {
     this.contas =  contasApi
   })
   }
+  deletar(id: number) {
+    Swal.fire({
+      title: 'Você tem certeza que deseja deletar?',
+      text: "Não tem como reverter essa ação",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: 'grey',
+      confirmButtonText: 'Deletar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.contasService.remover(id).subscribe(clientesApi => {
+          Swal.fire(
+            'Deletado',
+            'Cliente deletado com sucesso',
+            'success'
+          );
+          this.listarTodasContas()
+          console.log(id)
+        }, error => {
+          console.error(error)
+        })
+
+      }
+    })
+  }
+
+
 }
