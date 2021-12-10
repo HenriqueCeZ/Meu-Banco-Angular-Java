46e48c572858997c50c96b37073602571f90299d
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-10 06:20:59 -0300

    feat: importando cliente

--- view/meubanco/src/app/services/client.service.ts
+++ view/meubanco/src/app/services/client.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ICliente } from '../interfaces/cliente';
-import { Cliente } from '../pages/clientes/cadastrar-clientes/cliente';
+import { Cliente } from '../DTO/cliente';
 
 @Injectable({
   providedIn: 'root'
