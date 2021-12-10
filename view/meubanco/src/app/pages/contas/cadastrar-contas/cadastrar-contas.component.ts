bdc5ccef37f611a4bc624bf6676924f2a6f5c1ea
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-10 06:17:58 -0300

    feat: corrigindo componente de cadastro de contas para conseguir fazer o edit de contas

--- view/meubanco/src/app/pages/contas/cadastrar-contas/cadastrar-contas.component.ts
+++ view/meubanco/src/app/pages/contas/cadastrar-contas/cadastrar-contas.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-import { FormBuilder,FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {  ActivatedRoute, Router } from '@angular/router';
 import { ContasService } from 'src/app/services/contas.service';
-import { Contas } from '../contas';
+import { ClientService } from 'src/app/services/client.service';
+import { IContas } from 'src/app/interfaces/contas';
+import { ICliente } from 'src/app/interfaces/cliente';
+import { Contas } from 'src/app/DTO/contas';
 
 
 
@@ -15,74 +18,99 @@ import { Contas } from '../contas';
 export class CadastrarContasComponent implements OnInit  {
 
 
-  form!: FormGroup;
+  clientes: ICliente[] = [];
+  id!: number;
+  conta!: Contas;
+  idEdit!: number;
+  form: FormGroup;
 
 
-  constructor(
-
-    private formBuilder: FormBuilder,
-    public activatedRoute: ActivatedRoute,
-    private contasService: ContasService,
-    public router: Router,
 
 
-  ) {
 
-  }
+  constructor(form: FormBuilder,  private contaService: ContasService, private router: Router, private clienteService: ClientService, private activatedRoute: ActivatedRoute, private route: ActivatedRoute) {
+    this.form = form.group({
+      id: new FormControl(null),
+      agencia: new FormControl('', Validators.required),
+      cliente: new FormControl('', Validators.required),
+      numero: new FormControl('', Validators.required),
+      saldo: new FormControl('', Validators.required),
 
-  ngOnInit() {
-    this.createFormGoup();
-  }
-
-  createFormGoup() {
-    this.form = this.formBuilder.group({
-      agencia             : new FormControl('', [Validators.required]),
-      numero              : new FormControl('', [Validators.required]),
-      saldo               : new FormControl(0, [Validators.required]),
-      nomeCliente         : new FormControl('', ),
-      cpfCliente          : new FormControl('', ),
-      emailCliente        : new FormControl('', ),
-      observacoesCliente  : new FormControl(''),
-      id : new FormControl(),
 
     });
+    this.conta = new Contas();
+   }
+
+  ngOnInit(): void {
+    if(this.id = this.route.snapshot.params['id']){
+      this.contaService.getClientById(this.id).subscribe((data: Contas)=>{
+        this.conta = data;
+
+      });
+    }else{
+      this.listarTodosClientes();
+
+    }
+
+
   }
+  getCLientById() {
+    let id = this.form.get('cliente')?.value
+    let cliente
+    for (let element of this.clientes) {
+      if (element.id == id) {
+        cliente = element;
+        break;
+      }
+    }
+    return cliente
+  }
+
+  listarTodosClientes() {
+    this.clienteService.listarTodosClientes().subscribe(clientesApi => {
+    this.clientes  = clientesApi;
+    });
+  }
+
+
 
   onSubmit() {
- this.create()
-  }
 
-  create() {
-    const entity = this.formToConta();
-    console.log(entity)
-    this.contasService.create(entity).subscribe(clienteApi =>{
-      Swal.fire("Cadastro realizado!", "nova conta criada!","success")
-      console.log(clienteApi)
-      this.router.navigate(['/'])
-       },error =>{
-      console.log(error)
-     })
-  }
+    if(this.id){
+      this.contaService.atualizar(this.conta).subscribe(response =>{
+        Swal.fire("Cliente Atualizado!", "cliente atualizado com sucesso!","success")
+        console.log(response)
+        this.router.navigate(['/contas'])
 
-  formToConta(): Contas {
-    const formValue = this.form.value;
-    const entity = new Contas({
-      agencia       : formValue.agencia,
-      numero        : formValue.numero,
-      saldo         : formValue.saldo,
-      cliente: {
-        id          : formValue.id,
-        nome        : formValue.nomeCliente,
-        cpf         : formValue.cpfCliente,
-        email       : formValue.emailCliente,
-        observacoes : formValue.observacoesCliente,
-        ativo       : false,
+      },error =>{
+        Swal.fire({
+          icon: 'error',
+          title: 'Erro ao atualizar',
+          text: `${error}`
+        })
+      })
+    }
+  else{
+    let conta: IContas = this.form.value;
+    conta.cliente = this.getCLientById()!
+    this.contaService.create(conta).subscribe(contaApi => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Sucesso',
+        text: 'Conta cadastrada com sucesso',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      this.router.navigate(['/contas']);
+    }, error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro ao cadastrar',
+        text: `${error}`
+      })
+    })
 
-      }
-    });
-    return entity;
-  }
 
 }
-
-
+}
+}
